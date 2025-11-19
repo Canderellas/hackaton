1bb000618d1123762e185e8af504f4d0fd544f19
Canderellas <ibraevtemurr@gmail.com>
2025-11-20 05:30:26 +0700

    Красивый интерфейс

--- Frontend/src/components/QrCameraScanner.vue
+++ Frontend/src/components/QrCameraScanner.vue
@@ -2,7 +2,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
 import jsQR from 'jsqr'
-import QrResultModal from { "./QrResultModal"}
+import QrResultModal from './QrResultModal.vue'
 
 const video = ref(null)
 const canvas = ref(null)
